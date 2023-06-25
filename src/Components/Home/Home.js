27a8ffde5e9117e089bb19ca06356ba852f0cbf6
anacatarinafg <anafrontenddeveloper@gmail.com>
2023-06-25 18:04:22 +0100

    add scrolltrigger

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -5,8 +5,9 @@ import './home.css';
 const Home = () => {
   return (
     <div className='home'>
-      <h1 className='home__headline'>Ana</h1>
-      <p className='home__info'>My name is Ana. I'm a front-end developer based in Portugal.</p>
+      <h1 className='home__headline'></h1>
+      <p className='home__info'>
+      </p>
     </div>
 
   )
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,38 +1,54 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { gsap } from 'gsap';
+import ScrollTrigger from 'gsap/ScrollTrigger'; 
 import { Link } from 'react-router-dom';
 import './navbar.css';
 
 const Navbar = ({ style }) => {
+  useEffect(() => {
+    gsap.registerPlugin(ScrollTrigger); 
 
+    ScrollTrigger.create({
+      animation: gsap.from(".navbar__logotype", {
+        y: "50vh",
+        scale: 6,
+        yPercent: -50,
+      }),
+      scrub: true,
+      trigger: ".content",
+      start: "top bottom",
+      endTrigger: ".content",
+      end: "top center",
+    });
+  }, []);
   return (
-    <nav className="navbar" style={style}>
-      <div className="navbar__wrapper">
-        <div className="navbar__menu">
-          <div className="navbar__shrink">
-            <div className="navbar__left">
-              <div className="navbar__logo">
-                <Link to="/">ag</Link>
-              </div>
-            </div>
-            <div className="navbar__links">
-              <Link to="/about">About</Link>
-              <Link to="/work">Work</Link>
-              <Link to="/contact">Contact</Link>
-            </div>
+
+    <>
+      <nav className="navbar" style={style}>
+        <div className='navbar__wrapper'>
+          <div className='navbar__left'>
+            <ul className='navbar__list'>
+              <li className='navbar__item'><Link to="/">Index</Link></li>
+              <li className='navbar__item'><Link to="/about">About</Link></li>
+            </ul>
           </div>
-          <div className="navbar__expand">
-            <span className="divider"></span>
-            <div className="expand row-1">
-              <p>Ana Gonçalves</p>
-              <span>Portugal</span>
-            </div>
-            <div className="expand row-2">
-              <p>junior front-end developer</p>
-            </div>
+          <div className='navbar__right'>
+            <ul className='navbar__list'>
+              <li className='navbar__item'><Link to="/work">Work</Link></li>
+              <li className='navbar__item'><Link to="/contact">Contact</Link></li>
+            </ul>
           </div>
         </div>
+      </nav>
+      <div className='navbar__logotypeBox'>
+        <span className='navbar__logotype'>ag.</span>
       </div>
-    </nav>
+      <div className='container'></div>
+      <div className='content'>
+        <img src='https://images.unsplash.com/photo-1682687980961-78fa83781450?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80' alt='me'></img>
+      </div>
+    </>
+
   );
 };
 
