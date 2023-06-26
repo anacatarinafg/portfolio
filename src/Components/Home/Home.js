7b65b968cb25fe5e441517964ea5963f7a3f5097
anacatarinafg <anafrontenddeveloper@gmail.com>
2023-06-27 00:08:08 +0100

    update code

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -27,9 +27,6 @@ const Home = () => {
   }, []);
   return (
     <div className='home'>
-      <div className='navbar__logotypeBox'>
-        <span className='navbar__logotype'>ag.</span>
-      </div>
       <div className='container'></div>
       <div className='content'>
         <img src='https://images.unsplash.com/photo-1682687980961-78fa83781450?ixlib=rb-4.0.3&ixid=M3wxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=687&q=80' alt='me'></img>
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,10 +1,37 @@
 import React, { useEffect } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
+import gsap from 'gsap';
+import ScrollTrigger from 'gsap/ScrollTrigger';
 import './navbar.css';
 
 const Navbar = ({ style }) => {
-  return (
+  const location = useLocation();
+  const isHome = location.pathname === '/';
 
+  useEffect(() => {
+    if (!isHome) {
+      gsap.registerPlugin(ScrollTrigger);
+
+      ScrollTrigger.create({
+        animation: gsap.from('.navbar__logotype', {
+          y: '0vh',
+          scale: 6,
+          yPercent: 0,
+        }),
+        toggleClass: {
+          targets: '.navbar__logotype',
+          className: 'navbar__logotype--shrink',
+        },
+        scrub: true,
+        trigger: '.content',
+        start: 'top center',
+        endTrigger: '.content',
+        end: 'top top',
+      });
+    }
+  }, [isHome]);
+
+  return (
     <>
       <nav className="navbar" style={style}>
         <div className='navbar__wrapper'>
@@ -22,8 +49,10 @@ const Navbar = ({ style }) => {
           </div>
         </div>
       </nav>
+      <div className={`navbar__logotypeBox ${isHome ? 'home' : ''}`}>
+        <span className='navbar__logotype'>ag.</span>
+      </div>
     </>
-
   );
 };
 
