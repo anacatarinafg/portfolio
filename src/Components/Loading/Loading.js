2436538c6af5352a35d61d69d7207bf47684a053
anacatarinafg <anafrontenddeveloper@gmail.com>
2023-06-29 13:59:47 +0100

    fixing some loading errors

--- src/Components/Loading/Loading.js
+++ src/Components/Loading/Loading.js
@@ -5,15 +5,7 @@ import "./loading.css";
 
 const Loading = () => {
     useEffect(() => {
-        gsap.from("h2 div", 1.5, {
-            yPercent: 100,
-            ease: "power4.inOut",
-            stagger: {
-                amount: 0.5,
-            }
-        });
-
-        gsap.to("h2", 1.5, {
+        gsap.to(".has__state", 1.5, {
             clipPath: "polygon(0 0, 100% 0, 100% 100%, 0 100%)",
             ease: "power4.inOut",
             stagger: {
@@ -23,15 +15,7 @@ const Loading = () => {
 
         const overlay = document.querySelector(".overlay");
         overlay.addEventListener("click", function () {
-            gsap.to("h2 div", 1.5, {
-                yPercent: -100,
-                ease: "power4.inOut",
-                stagger: {
-                    amount: 0.5,
-                }
-            });
-
-            gsap.to("h2", 1.5, {
+            gsap.to(".has__state", 1.5, {
                 clipPath: "polygon(0 85%, 100% 85%, 100% 100%, 0 100%)",
                 ease: "power4.inOut",
                 stagger: {
@@ -49,17 +33,17 @@ const Loading = () => {
         <div className='overlay'>
             <div className='overlay__info'>
                 <div className='overlay__name'>
-                    <h2>
+                    <h2 className='has__state'>
                         Ana Gonçalves
                     </h2>
                 </div>
                 <div className='overlay__job'>
-                    <p>Junior front-end developer</p>
+                    <p className='has__state'>Junior front-end developer</p>
                 </div>
             </div>
             <div className='overlay__click'>
-                <span>click</span>
-                <p>anywhere to continue</p>
+                <span className='has__state'>click</span>
+                <p className='has__state'>anywhere to continue</p>
             </div>
         </div>
     )
