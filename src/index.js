cd23553973c2cda72c00b91227534d9e93149c4d
anacatarinafg <anafrontenddeveloper@gmail.com>
2023-07-05 12:34:48 +0100

    update the images

--- src/index.js
+++ src/index.js
@@ -10,13 +10,11 @@ root.render(
   <React.StrictMode>
     <Router>
       <Routes>
-        <Route path='/' element={<App />}></Route>
+        <Route path='/*' element={<App />}></Route>
       </Routes>
     </Router>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
 reportWebVitals();
