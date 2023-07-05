a72f6244598dc8bfc3643aa2b8e02282c21f643c
anacatarinafg <anafrontenddeveloper@gmail.com>
2023-07-05 12:41:30 +0100

    trying to fix an error

--- src/index.js
+++ src/index.js
@@ -1,20 +1,19 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <Router>
       <Routes>
-        <Route path='/*' element={<App />}></Route>
+        <Route path='/*' element={<App />} />
       </Routes>
     </Router>
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root')
 );
-
 
 reportWebVitals();
