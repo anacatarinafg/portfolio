6aa0c5acaea890d44ded3b21629a5ad7e41c32b9
anacatarinafg <anafrontenddeveloper@gmail.com>
2023-07-12 19:36:40 +0100

    responsive navbar

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,18 +1,51 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import { Link } from 'react-router-dom';
 import './navbar.css';
+import { GiHamburgerMenu } from "react-icons/gi";
+import { AiOutlineClose } from "react-icons/ai";
 
 const Navbar = ({ style }) => {
+  const navbarRef = useRef(null);
 
+  useEffect(() => {
+    const openNavbar = document.querySelector("#navbar__button");
+    const closeNavbar = document.querySelector("#navbar__closeButton");
+    const navbar = navbarRef.current;
+  
+    const openNav = () => {
+      if (navbar) {
+        navbar.classList.add("visible");
+      }
+    };
+  
+    const closeNav = () => {
+      if (navbar) {
+        navbar.classList.remove("visible");
+      }
+    };
+  
+    if (openNavbar && closeNavbar) {
+      openNavbar.addEventListener("click", openNav);
+      closeNavbar.addEventListener("click", closeNav);
+  
+      return () => {
+        openNavbar.removeEventListener("click", openNav);
+        closeNavbar.removeEventListener("click", closeNav);
+      };
+    }
+  }, []);
 
   return (
     <>
-      <nav className="navbar" style={style} >
-        <div className='navbar__wrapper'>
+      <nav className="navbar" style={style}>
+        <div className='navbar__wrapper' ref={navbarRef}>
+          <button className="navbar__closeButton" id="navbar__closeButton">
+            <AiOutlineClose />
+          </button>
           <div className='navbar__left'>
             <ul className='navbar__list'>
-              <li className='navbar__item'><Link to="/portfolio">Index</Link></li>
-              <li className='navbar__item'><Link to="/about">About</Link></li>
+              <li className='navbar__item'><Link className="navbar__link" to="/portfolio">Index</Link></li>
+              <li className='navbar__item'><Link className="navbar__link" to="/about">About</Link></li>
             </ul>
           </div>
           <div className="navbar__logotypeBox">
@@ -20,12 +53,15 @@ const Navbar = ({ style }) => {
           </div>
           <div className='navbar__right'>
             <ul className='navbar__list'>
-              <li className='navbar__item'><Link to="/work">Work</Link></li>
-              <li className='navbar__item'><Link to="/contact">Contact</Link></li>
+              <li className='navbar__item'><Link className="navbar__link" to="/work">Work</Link></li>
+              <li className='navbar__item'><Link className="navbar__link" to="/contact">Contact</Link></li>
             </ul>
           </div>
         </div>
       </nav>
+      <button id="navbar__button" className="navbar__button navbar__openButton">
+        <GiHamburgerMenu />
+      </button>
     </>
   );
 };
